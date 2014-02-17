f2172a86479dfc42b1915af31533985a5aadaa05
Chris Sloey <chris.sloey@gmail.com>
2014-02-17 14:12:07 +0000

    Add details to User model

---
+++ db/migrate/20140217140953_add_details_to_user.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :profile_picture, :string
+    add_column :users, :gender, :integer
+    add_column :users, :dob, :date
+  end
+end
