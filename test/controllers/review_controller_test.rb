ba7969f1d4136f0b27964b15f4293cc7955ac079
Chris Sloey <chris.sloey@gmail.com>
2014-02-18 11:20:38 +0000

    Test review controller

--- test/controllers/review_controller_test.rb
+++ test/controllers/review_controller_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class ReviewControllerTest < ActionController::TestCase
-  # test "should get create" do
-  #   post :create
-  #   assert_response :success
-  # end
+  test "should create review" do
+    route = Route.first
+    review = {
+      safety_rating: 5,
+      difficulty_rating: 3,
+      environment_rating: 4,
+      comment: "Great route, A+++++"
+    }
+
+    review_count = route.reviews.count
+    post :create, route_id: route.id, review: review
+    assert_response :success, "Should succeed POSTing correct params to review#create"
+
+    review_data = JSON.parse response.body
+    assert_not_nil review_data, "Review json should be returned"
+
+    assert_equal review_count + 1, route.reviews.count, "Review should be added to route"
+
+    review_id = review_data['review_id']
+    assert_not_nil review_id, "Review id should be returned"
+    assert_not_nil RouteReview.where(id: review_id).first, "Review should exist with returned id"
+  end
+
+  test "should fail to create a review when no params specified" do
+    post :create
+    assert_response :bad_request
+  end
 
   # test "should get retrieve" do
   #   get :find
