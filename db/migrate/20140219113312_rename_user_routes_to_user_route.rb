79e60c7a09a42235ec72a71f45345de5d09bf69a
Chris Sloey <chris.sloey@gmail.com>
2014-02-19 14:09:58 +0000

    Remove route point model

--- db/migrate/20140219113312_rename_user_routes_to_user_route.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserRoutesToUserRoute < ActiveRecord::Migration
-  def change
-  	rename_table :user_routes, :user_route
-  end
-end
--- db/migrate/20140219115050_rename_user_route_to_user_routes.rb
+++
@@ -1,5 +0,0 @@
-class RenameUserRouteToUserRoutes < ActiveRecord::Migration
-  def change
-  	rename_table :user_route, :user_routes
-  end
-end
