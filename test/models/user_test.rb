ddfafffee6628cc10f3b20ab691cd69edefe4ed0
mdhayes <mdhayes@gmx.com>
2014-03-03 16:42:22 +0000

    user#details tests now test all expected data

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -13,17 +13,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   #Need to test for when the user has no routes
-  test "Route details are as expected" do
-    user = create(:user)
+  test "user details are as expected" do
+    user = create(:user, id: 1, first_name: "Test", last_name: "McTester")
     parent_route = create(:route, user_id: user.id, total_distance: 100, total_time: 0)
     child_routes = create_list(:route, 5, route_id: parent_route.id, name: "jobby", user_id: user.id, total_time: 100, total_distance: 100)
     other_routes = create_list(:route, 5, user_id: user.id, total_distance: 0, total_time: 0)
 
+    assert_equal 1, user.details[:user_id], "User id not as expected"
+    assert_equal "Test", user.details[:first_name], "User first_name not as expected"
+    assert_equal "McTester", user.details[:last_name], "User last_name not as expected"
     assert_equal 11, user.details[:month][:total], "Total routes should be correct"
     assert_equal "jobby", user.details[:month][:route], "Favourite route should be correct"
     assert_equal 600, user.details[:month][:meters], "Total meters should be correct"
     assert_equal 500, user.details[:month][:seconds], "Total seconds route should be correct"
-
   end
-
 end
