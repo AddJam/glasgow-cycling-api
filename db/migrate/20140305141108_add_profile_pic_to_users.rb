9de9d728f4b615af04a4d55c5738b61079640964
Chris Sloey <chris.sloey@gmail.com>
2014-03-05 14:16:44 +0000

    Add profile_pic attachment to user model

---
+++ db/migrate/20140305141108_add_profile_pic_to_users.rb
@@ -0,0 +1,10 @@
+
+class AddProfilePicToUsers < ActiveRecord::Migration
+  def up
+    add_attachment :users, :profile_pic
+  end
+
+  def down
+    remove_attachment :users, :profile_pic
+  end
+end
