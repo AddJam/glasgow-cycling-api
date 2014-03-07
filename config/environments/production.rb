8082c8082460d850524a286513cdfd26609f971b
Chris Sloey <chris.sloey@gmail.com>
2014-03-07 14:41:56 +0000

    Remove paperclip configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,4 @@ JourneyAPI::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-
-  Paperclip.options[:command_path] = "/usr/bin/"
 end
