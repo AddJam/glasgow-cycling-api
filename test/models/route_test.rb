2d5164d201ed04463359e260b18231e7a7e909cb
Chris Sloey <chris.sloey@gmail.com>
2014-04-09 14:55:09 +0100

    Fix factory errors with RouteTest - tests now all pass OOTB with a clean DB

--- test/models/route_test.rb
+++ test/models/route_test.rb
@@ -1,9 +1,24 @@
 require 'test_helper'
 
 class RouteTest < ActiveSupport::TestCase
+  def route_point_params(num_points, point_attrs = {})
+    points = []
+    num_points.times do
+      new_point = {
+        lat: rand * 100,
+        long: rand * 100,
+        altitude: rand * 500,
+        time: Time.now,
+        kph: rand * 500
+      }
+      new_point.merge!(point_attrs)
+      points << new_point
+    end
+    points
+  end
 
   test "recording a route should store the route and all route points" do
-    points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), time: Time.now)
+    points = route_point_params(3)
     route = Route.record(create(:user), points)
 
     assert_not_nil route, "Route created by record"
@@ -12,7 +27,7 @@ class RouteTest < ActiveSupport::TestCase
 
   test "recording a route should add it to the users routes" do
     user = create(:user)
-    route_points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), time: Time.now)
+    route_points = route_point_params(3)
     route = Route.record(user, route_points)
     route_added_to_user = user.routes.include? route
     assert route_added_to_user, "route should be added to user routes after recording"
@@ -20,15 +35,15 @@ class RouteTest < ActiveSupport::TestCase
 
   test "recording a route should store route time" do
     user = create(:user)
-    points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), time: Time.now)
+    points = route_point_params(3)
     route = Route.record(user, points)
-    total_time_seconds = points.last[:time] - points.first[:time]
+    total_time_seconds = route.points.last[:time] - route.points.first[:time]
     assert_equal total_time_seconds, route.total_time, "store route time in user history"
   end
 
   test "can add route use to existing route" do
     user = create(:user)
-    points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), kph: (rand * 500), time: Time.now)
+    points = route_point_params(3)
 
     original = Route.record(user, points)
     route_use = original.record_use(user, points)
@@ -45,7 +60,7 @@ class RouteTest < ActiveSupport::TestCase
     # Note - distance is calculated once for a route
     #        after this, it is assumed points wont be modified
     user = create(:user)
-    points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), time: Time.now)
+    points = route_point_params(3)
 
     points[0][:lat] = 0.0
     points[0][:long] = 0.0
@@ -85,7 +100,7 @@ class RouteTest < ActiveSupport::TestCase
   end
 
   test "route created and mode default to bike enum" do
-    points = create_list(:route_point, 3, lat: (rand * 100), long: (rand * 100), altitude: (rand * 500), time: Time.now)
+    points = route_point_params(3)
 
     route = Route.record(create(:user), points)
     assert_equal "bike", route.mode, "Route mode should default to 0 (bike)"
