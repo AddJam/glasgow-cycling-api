a9ea8cd5078a47d1ee64dc5e3ebc07a69adbe287
Chris Sloey <chris.sloey@gmail.com>
2014-04-10 14:54:20 +0100

    Remove unused code

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,8 +2,6 @@ Geocoder.configure(
 
     # geocoding service
     :lookup => :nominatim,
-    # :lookup => :mapquest,
-    # :api_key => "Fmjtd%7Cluur2968nq%2Cbw%3Do5-90rs50",
 
     # set default units to kilometers:
     :units => :km,
