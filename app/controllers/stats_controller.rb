3019388f278625a994680f3f5297512a4759cdc1
Chris Sloey <chris.sloey@gmail.com>
2014-05-19 10:52:41 +0100

    Fix user auth in stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,7 +1,12 @@
 class StatsController < ApplicationController
+  # This is our new function that comes before Devise's one
+  before_filter :authenticate_user_from_token!
+  # This is Devise's authentication
+  before_filter :authenticate_user!
+
   def hours
     unless current_user and params[:num_hours]
-      render status: :bad_request, json: {error: "must be signed in and specify num_hours in paramters"}
+      render status: :bad_request, json: {error: "must be signed in and specify num_hours in parameters"}
     else
       stats = Hour.hours(params[:num_hours].to_i, current_user)
       if stats
@@ -14,7 +19,7 @@ class StatsController < ApplicationController
 
   def days
     unless current_user and params[:num_days]
-      render status: :bad_request, json: {error: "must be signed in and specify num_days in paramters"}
+      render status: :bad_request, json: {error: "must be signed in and specify num_days in parameters"}
     else
       stats = Hour.days(params[:num_days].to_i, current_user)
       if stats
@@ -27,7 +32,7 @@ class StatsController < ApplicationController
 
   def weeks
     unless current_user and params[:num_weeks]
-      render status: :bad_request, json: {error: "must be signed in and specify num_weeks in paramters"}
+      render status: :bad_request, json: {error: "must be signed in and specify num_weeks in parameters"}
     else
       stats = Hour.weeks(params[:num_weeks].to_i, current_user)
       if stats
