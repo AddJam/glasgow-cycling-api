b1c380bec9342ce7e5868f80bdc4cc1e6bf0469e
Chris Sloey <chris.sloey@gmail.com>
2014-05-29 11:55:44 +0100

    Add foreman export to upstart in deploy.rb

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 redis: redis-server
-web: bundle exec rails server
 sidekiq: bundle exec sidekiq
 clock: bundle exec clockwork lib/clock.rb
--- config/deploy.rb
+++ config/deploy.rb
@@ -54,3 +54,28 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+namespace :foreman do
+  desc "Export the Procfile to Ubuntu's upstart scripts"
+  task :export, :roles => :app do
+    run "cd #{current_path} && sudo bundle exec foreman export upstart /etc/init -a #{application} -u #{user} -l #{shared_path}/log"
+  end
+
+  desc "Start the application services"
+  task :start, :roles => :app do
+    sudo "start #{application}"
+  end
+
+  desc "Stop the application services"
+  task :stop, :roles => :app do
+    sudo "stop #{application}"
+  end
+
+  desc "Restart the application services"
+  task :restart, :roles => :app do
+    run "sudo start #{application} || sudo restart #{application}"
+  end
+end
+
+after "deploy:update", "foreman:export"
+after "deploy:update", "foreman:restart"
