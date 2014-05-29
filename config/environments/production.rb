e80570952519469cb9676e9c1d7fa2cf2fc00d83
Chris Sloey <chris.sloey@gmail.com>
2014-05-29 15:36:14 +0100

    Production mail with mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,21 @@ JourneyAPI::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Devise needs...
+  config.action_mailer.default_url_options = { :host => 'activetravel.cloudapp.net' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.mandrillapp.com",
+    :port => 25, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => Rails.application.secrets.mandrill_username,
+    :password => Rails.application.secrets.mandrill_api_key,
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'activetravel.cloudapp.net', # your domain to identify your server when connecting
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
