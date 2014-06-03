eac733d90d0d1962352c4fac4a004685bd50ea95
Chris Sloey <chris.sloey@gmail.com>
2014-06-03 13:51:44 +0100

    Remove secrets.yml linking from deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,6 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/symetric-encryption.yml #{release_path}/config/symetric-encryption.yml"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
