f4f9de93eefbe860ab42f5de7a4acb8559af8efe
Chris Sloey <chris.sloey@gmail.com>
2014-07-02 14:14:11 +0100

    Remote auth from token method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,6 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def authenticate_user_from_token!
-    user_email = params[:user_email].presence
-    user = user_email && User.find_by_email(user_email.downcase)
-
-    # Notice how we use Devise.secure_compare to compare the token
-    # in the database with the token given in the params, mitigating
-    # timing attacks.
-    if user && Devise.secure_compare(user.authentication_token, params[:user_token])
-      sign_in user, store: false
-    end
-  end
-
   def current_user
     if doorkeeper_token
       return current_resource_owner
