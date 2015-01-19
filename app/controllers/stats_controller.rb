ed43ea9549cd55e498f1190169ebcfd2d5cbc4ec
Chris Sloey <chris.sloey@gmail.com>
2015-01-19 12:12:22 +0000

    Add caching to stats overview

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -52,50 +52,53 @@ class StatsController < ApplicationController
       days = 28
     end
 
-    period_start = (days-1).days.ago.beginning_of_day
-    period_start = (13).days.ago.beginning_of_day
-    hours = Hour.where('time >= ? AND time <= ?', period_start, Time.now).order(:time)
+    data = Rails.cache.fetch("stats-overvoew-#{days}-days-#{filter}", expires_in: 15.minutes) do
+      period_start = (days-1).days.ago.beginning_of_day
+      hours = Hour.where('time >= ? AND time <= ?', period_start, Time.now).order(:time)
 
-    segments = []
-    segment_ids = *(0...(24*days))
-    segment_ids.each do |i|
-      seconds_in_hour = 3600
-      hour_start = period_start.to_i + (i*seconds_in_hour)
-      matching_hours = hours.select {|h| h.time.to_i == hour_start}
-      segments << matching_hours.inject({
-        id: i,
-        time: hour_start,
-        distance: 0,
-        routes: 0
-      }) do |segment, hour|
-        segment[:distance] += hour.distance
-        segment[:routes] += hour.routes_completed
-        segment
+      segments = []
+      segment_ids = *(0...(24*days))
+      segment_ids.each do |i|
+        seconds_in_hour = 3600
+        hour_start = period_start.to_i + (i*seconds_in_hour)
+        matching_hours = hours.select {|h| h.time.to_i == hour_start}
+        segments << matching_hours.inject({
+          id: i,
+          time: hour_start,
+          distance: 0,
+          routes: 0
+        }) do |segment, hour|
+          segment[:distance] += hour.distance
+          segment[:routes] += hour.routes_completed
+          segment
+        end
       end
+
+      new_cyclists = User.where('created_at > ?', period_start).count
+      active_cyclists = hours.map {|hour| hour.user_id}.uniq.count
+
+      routes = Route.where('start_time >= ?', period_start)
+      total_distance = Route.sum(:total_distance)
+
+      {
+        overviews: [{
+          id: filter,
+          totalCyclists: User.count,
+          activeCyclists: active_cyclists,
+          newCyclists: new_cyclists,
+          distance: hours.pick(:distance).sum,
+          totalDistance: total_distance,
+          duration: hours.pick(:duration).sum,
+          routes: hours.pick(:routes_completed).sum,
+          longestRoute: routes.pick(:total_distance).max,
+          avgDistancePerUser: -1,
+          avgDistancePerRoute: routes.pick(:total_distance).average,
+          segments: segment_ids
+        }],
+        segments: segments
+      }
     end
 
-    new_cyclists = User.where('created_at > ?', period_start).count
-    active_cyclists = hours.map {|hour| hour.user_id}.uniq.count
-
-    routes = Route.where('start_time >= ?', period_start)
-    total_distance = Route.sum(:total_distance)
-
-    render json: {
-      overviews: [{
-        id: filter,
-        totalCyclists: User.count,
-        activeCyclists: active_cyclists,
-        newCyclists: new_cyclists,
-        distance: hours.pick(:distance).sum,
-        totalDistance: total_distance,
-        duration: hours.pick(:duration).sum,
-        routes: hours.pick(:routes_completed).sum,
-        longestRoute: routes.pick(:total_distance).max,
-        avgDistancePerUser: -1,
-        avgDistancePerRoute: routes.pick(:total_distance).average,
-        segments: segment_ids
-      }],
-      segments: segments
-    }
+    render json: data
   end
 end
